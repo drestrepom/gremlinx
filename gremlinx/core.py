db6684fdab339b6939ee77c718db51292c74ba3d
Diego Restrepo Mesa <36453706+Skouro@users.noreply.github.com>
2020-11-20 17:41:06 -0500

    refac(back): recreate has label (#14)

--- gremlinx/core.py
+++ gremlinx/core.py
@@ -1,18 +1,32 @@
 # Standar library
 from __future__ import annotations
 from typing import (
-    Union, )
+    Any,
+    Callable,
+    Dict,
+    List,
+    Generator,
+    Optional,
+    Tuple,
+    Union,
+)
+from enum import Enum
 
 # Third libraries
 from networkx import (
     Graph,
     DiGraph,
+    dfs_successors,
 )
+import networkx as nx
 import rx
 from rx.core import Observable
+import rx.operators as ops
 
 # Local imports
+from gremlinx.utils.exceptions import NotExecutable
 from gremlinx.utils.classes import SourceType
+from gremlinx.utils import statics
 
 
 class GraphTraversalSource():
@@ -57,3 +71,54 @@ class GraphTraversal():
         self.graph = graph
         self.sources = sources
         self.source_type = source_type
+
+    @property
+    def sources_is_vertex(self) -> bool:
+        return self.source_type == SourceType.VERTEX
+
+    @property
+    def sources_is_edges(self) -> bool:
+        return self.source_type == SourceType.EDGE
+
+    def _has_label(self, vertex: str, *labels: str) -> bool:
+        vertex_data = self.graph.nodes[vertex]
+        return all(vertex_data.get(f'label_{label}')
+                   for label in labels) or all(
+                       any(
+                           key.startswith('label') and value == label
+                           for key, value in vertex_data.items())
+                       for label in labels)
+
+    def hasLabel(self, *labels: str) -> GraphTraversal:
+        """Filter the nodes that have the label.
+
+        Networkx does not have native support for labels, they must be,
+        simulated with node properties.
+
+        To filter the nodes by labels, the labels must have the following
+        format.
+
+        >>> G.addNode('1', labelV='airport')
+
+        The key of the label must have the following format `label[anything]`.
+        >>> # Find nodes that are airports
+        >>> g.V().hasLabel('airport')
+
+        Args:
+            labels (*str): labels values.
+
+        Raises:
+            NotExecutable: this function can only be executed on nodes.
+
+        Returns:
+            GraphTraversal: nodes that contain the indicated label.
+        """
+        if self.source_type == SourceType.EDGE:
+            raise NotExecutable
+
+        return GraphTraversal(
+            sources=self.sources.pipe(
+                ops.filter(lambda x: statics.hasLabel(self, x, *labels))),
+            graph=self.graph,
+            source_type=self.source_type,
+        )
