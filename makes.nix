78cd6f6ecd7c161b7e77a985cfc42a599550876e
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-23 22:21:45 -0500

    feat(build): drestrepom#29 add makes
    - add makes basic configuration
    - add runtime using python packaging
    - add basic linter

---
+++ makes.nix
@@ -0,0 +1,16 @@
+{
+  lintPython = {
+    dirsOfModules = {
+      makes = {
+        python = "3.8";
+        src = "/gremlinx/";
+      };
+    };
+    modules = {
+      cliMain = {
+        python = "3.8";
+        src = "/gremlinx/";
+      };
+    };
+  };
+}
