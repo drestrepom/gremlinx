2f7b5adb7493d1070d90a289d42837ce5ce2e256
Diego Restrepo <restrepomesadiego@gmail.com>
2021-09-26 18:11:59 -0500

    feat(back): add graph traversal base

--- gremlinx/utils/classes.py
+++ gremlinx/utils/classes.py
@@ -1,8 +1,35 @@
 from enum import (
     Enum,
 )
+from networkx.classes.digraph import (
+    DiGraph,
+)
+from networkx.classes.graph import (
+    Graph,
+)
+from rx.core.typing import (
+    Observable,
+)
+from typing import (
+    List,
+    Tuple,
+    Union,
+)
 
 
 class SourceType(Enum):
     VERTEX = "Vertex"
     EDGE = "EDGE"
+
+
+class GraphTraversalBase:
+    def __init__(
+        self,
+        graph: Union[Graph, DiGraph],
+        sources: Observable[Tuple[str, ...]],
+        source_type: SourceType,
+    ):
+        self.graph = graph
+        self.sources = sources
+        self.source_type = source_type
+        self.__results__: List[str] = []
